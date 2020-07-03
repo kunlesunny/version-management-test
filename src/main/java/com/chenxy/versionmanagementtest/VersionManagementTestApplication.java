02dea0f0e7a42f67b7b0fda15abffa4b7f68d067
kunlesunny <2453039725@qq.com>
2020-07-03 22:40:55 +0800

    初始化项目

---
+++ src/main/java/com/chenxy/versionmanagementtest/VersionManagementTestApplication.java
@@ -0,0 +1,13 @@
+package com.chenxy.versionmanagementtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionManagementTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionManagementTestApplication.class, args);
+	}
+
+}
